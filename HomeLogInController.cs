33e7fdf737ad298bc26701f9bdff0d8ad2384ef2
Mahesh9191 <91869623+Mahesh9191@users.noreply.github.com>
2024-11-02 23:33:23 +0530

    Add files via upload

---
+++ HomeLogInController.cs
@@ -0,0 +1,89 @@
+﻿using CorePractice.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace CorePractice.Controllers
+{
+    public class HomeLogInController : Controller
+    {
+        private readonly LogInDbContext dbcontext;
+
+        public HomeLogInController(LogInDbContext Dbcontext)
+        {
+            this.dbcontext = Dbcontext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LogIn()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                return RedirectToAction("Dashboard");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult LogIn(LogIn login)
+        {
+            var mylogins=dbcontext.LogIns.Where(x=>x.Email==login.Email && x.Password==login.Password).FirstOrDefault();
+            
+            if (mylogins != null)
+            {
+                HttpContext.Session.SetString("UserSession", login.Email);
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                ViewBag.Message = "Login Failed";
+            }
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+           if( HttpContext.Session.GetString("UserSession") != null)
+            {
+                ViewBag.MySession=HttpContext.Session.GetString("UserSession").ToString();
+            }
+            else
+            {
+                return RedirectToAction("LogIn");
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                HttpContext.Session.Remove("UserSession");
+                return RedirectToAction("LogIn");
+            }
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LogIn login)
+        {
+            if (ModelState.IsValid)
+            {
+               await dbcontext.LogIns.AddAsync(login);
+                await dbcontext.SaveChangesAsync();
+                TempData["Success"] = "Registered Successfully..";
+                return RedirectToAction("LogIn");
+            }
+            return View();
+        }
+    }
+}
---
+++ LogInDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CorePractice.Models;
+
+public partial class LogInDbContext : DbContext
+{
+    public LogInDbContext()
+    {
+    }
+
+    public LogInDbContext(DbContextOptions<LogInDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<LogIn> LogIns { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<LogIn>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__LogIn__3214EC27F62A7272");
+
+            entity.ToTable("LogIn");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Gender)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
